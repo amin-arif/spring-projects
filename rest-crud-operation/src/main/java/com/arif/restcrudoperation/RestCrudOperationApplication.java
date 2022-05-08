d2bf98a89e9ee69169e3ab4ebe6b0903c8598707
amin-arif <aminarif.cs@gmail.com>
2022-05-08 11:52:20 +0600

    rest crud api project initialization

---
+++ rest-crud-operation/src/main/java/com/arif/restcrudoperation/RestCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.arif.restcrudoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudOperationApplication.class, args);
+	}
+
+}
---
+++ rest-crud-operation/src/test/java/com/arif/restcrudoperation/RestCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arif.restcrudoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
