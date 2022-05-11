54a430e6254d1329a77d85a091a965db11e3e54f
amin-arif <aminarif.cs@gmail.com>
2022-05-11 12:38:46 +0600

    User and Role are added

--- spring-security-with-jwt/src/main/java/com/arif/springsecuritywithjwt/SpringSecurityWithJwtApplication.java
+++ spring-security-with-jwt/src/main/java/com/arif/springsecuritywithjwt/SpringSecurityWithJwtApplication.java
@@ -1,13 +1,43 @@
 package com.arif.springsecuritywithjwt;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.arif.springsecuritywithjwt.model.Role;
+import com.arif.springsecuritywithjwt.model.User;
+import com.arif.springsecuritywithjwt.service.UserService;
 
 @SpringBootApplication
 public class SpringSecurityWithJwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityWithJwtApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role("ROLE_USER"));
+			userService.saveRole(new Role("ROLE_MANAGER"));
+			userService.saveRole(new Role("ROLE_ADMIN"));
+			userService.saveRole(new Role("ROLE_SUPER_ADMIN"));
+			
+			userService.saveUser(new User("Arif", "arif@gmail.com", "1234", new ArrayList<>()));
+			userService.saveUser(new User("Labib", "labib@gmail.com", "1234", new ArrayList<>()));
+			userService.saveUser(new User("Moin", "moin@gmail.com", "1234", new ArrayList<>()));
+			userService.saveUser(new User("Ashraful", "ashraful@gmail.com", "123456", new ArrayList<>()));
+			
+			userService.addRoleToUser("arif@gmail.com", "ROLE_USER");
+			userService.addRoleToUser("arif@gmail.com", "ROLE_MANAGER");
+			userService.addRoleToUser("labib@gmail.com", "ROLE_MANAGER");
+			userService.addRoleToUser("moin@gmail.com", "ROLE_ADMIN");
+			userService.addRoleToUser("ashraful@gmail.com", "ROLE_ADMIN");
+			userService.addRoleToUser("ashraful@gmail.com", "ROLE_SUPER_ADMIN");
+		};
 	}
 
 }
--- spring-security-with-jwt/src/main/java/com/arif/springsecuritywithjwt/controller/UserController.java
+++ spring-security-with-jwt/src/main/java/com/arif/springsecuritywithjwt/controller/UserController.java
@@ -1,22 +1,26 @@
 package com.arif.springsecuritywithjwt.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.arif.springsecuritywithjwt.model.Role;
 import com.arif.springsecuritywithjwt.model.User;
 import com.arif.springsecuritywithjwt.service.UserService;
-
-//import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
 	private final UserService userService;
+	
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
@@ -25,4 +29,53 @@ public class UserController {
 	public ResponseEntity<List<User>> getUsers() {
 		return ResponseEntity.ok().body(userService.getAllUsers());
 	}
+	
+	@PostMapping("/user/save")
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());		
+		return ResponseEntity.created(uri).body(userService.saveUser(user));
+	}
+	
+	@PostMapping("/role/save")
+	public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+		return ResponseEntity.created(uri).body(userService.saveRole(role));
+	}
+	
+	@PostMapping("/role/addtouser")
+	public ResponseEntity<?> saveRoleToUser(@RequestBody RoleToUserForm form) {
+		userService.addRoleToUser(form.getUserEmail(), form.getRoleName());
+		return ResponseEntity.ok().build();
+	}
+	
 }
+
+
+class RoleToUserForm {
+	
+	private String userEmail;
+	private String roleName;
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
+
+
+
+
+
+
+
+
+
+
