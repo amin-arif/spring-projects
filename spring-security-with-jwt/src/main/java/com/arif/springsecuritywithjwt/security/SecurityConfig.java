42cc2fef7040cdceabcc57b0fb1ff7122fb944e0
arif <mdarifkanon@gmail.com>
2022-05-16 06:47:01 +0600

    temporary finish jwt token with spring security

--- spring-security-with-jwt/src/main/java/com/arif/springsecuritywithjwt/security/SecurityConfig.java
+++ spring-security-with-jwt/src/main/java/com/arif/springsecuritywithjwt/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.arif.springsecuritywithjwt.security;
 
 import com.arif.springsecuritywithjwt.filter.CustomAuthenticationFilter;
+import com.arif.springsecuritywithjwt.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -21,9 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private final UserDetailsService userDetailsService;
 	private final BCryptPasswordEncoder bcryptPasswordEncoder;
 	
-//	public SecurityConfig(UserDetailsService userDetailsService) {
-//		this.userDetailsService = userDetailsService;
-//	}
+	/*public SecurityConfig(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}*/
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -32,10 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		CustomAuthenticationFilter authenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+		authenticationFilter.setFilterProcessesUrl("/api/login"); // change default url (/login)
+
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
-		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+		http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
+		http.authorizeRequests().antMatchers(GET, "/api/users/**").hasAnyAuthority("ROLE_USER");
+		http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+		http.authorizeRequests().anyRequest().authenticated();
+		http.addFilter(authenticationFilter);
+		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Bean
@@ -43,5 +55,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	
+
 }
