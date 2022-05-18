7b41eb00f0c9da9b6755a183d26404ac262f3cab
amin-arif <aminarif.cs@gmail.com>
2022-05-18 18:34:11 +0600

    Student entity added

---
+++ spring-data-jpa/src/main/java/com/arif/spring/data/jpa/entity/Student.java
@@ -0,0 +1,36 @@
+package com.arif.spring.data.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "student", uniqueConstraints = @UniqueConstraint(name = "email_unique", columnNames = "email_address"))
+public class Student {
+	
+	@Id
+	@SequenceGenerator(name = "st_id", sequenceName = "student_id", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "st_id")
+	private Long studentId;
+	
+	@Column(name = "student_name")
+	private String name;
+	
+	@Column(name = "email_address", nullable = false)
+	private String email;
+	
+}
