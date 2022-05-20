6345e563904d148ab8380407a3f169334f20c3ee
arif <mdarifkanon@gmail.com>
2022-05-20 23:55:55 +0600

    Test repo for studentRepo

---
+++ spring-data-jpa/src/test/java/com/arif/spring/data/jpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.arif.spring.data.jpa.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    StudentRepository studentRepository;
+}
