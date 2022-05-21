d1d503cd4e5ea3c5057c92cafc7af6dac2f4749d
arif <mdarifkanon@gmail.com>
2022-05-22 00:02:20 +0600

    Guardian model added

--- spring-data-jpa/src/test/java/com/arif/spring/data/jpa/repository/StudentRepositoryTest.java
+++ spring-data-jpa/src/test/java/com/arif/spring/data/jpa/repository/StudentRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.arif.spring.data.jpa.repository;
 
+import com.arif.spring.data.jpa.entity.Guardian;
 import com.arif.spring.data.jpa.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class StudentRepositoryTest {
@@ -14,10 +17,35 @@ class StudentRepositoryTest {
     @Test
     public void saveStudent() {
         Student student = Student.builder()
-                        .name("john")
-                                .email("john@email.com")
+                        .name("ted")
+                                .email("ted@email.com")
                                         .build();
 
         studentRepository.save(student);
     }
+
+    @Test
+    public void saveStudentWithGuardianInfo() {
+        Guardian guardian = Guardian.builder()
+                        .name("David")
+                                .email("david@gmail.com")
+                                        .phone("01612309090")
+                                                .build();
+
+        Student student = Student.builder()
+                .name("ted")
+                .email("ted@email.com")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void getAllStudent() {
+        List<Student> studentList = studentRepository.findAll();
+
+        System.out.println("Student List = " + studentList);
+    }
+
 }
